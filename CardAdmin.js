6880eb14da459013369a22e9f43f3b1e35ce3810
Khoi <Khoi@Apples-MacBook-Pro.local>
2017-04-07 18:45:56 -0700

    final release

--- CardAdmin.js
+++ CardAdmin.js
@@ -1,19 +1,23 @@
+//importing files.
 var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
+//arrays to store both types of cards.
 var storeBC = [];
 var storeCC = [];
 
+//main constructor to handle both types of cards.
 function CardAdmin (front, back, text, cloze, choice) {
   this.front = front;
   this.back = back;
   this.text = text;
   this.cloze = cloze;
   this.choice = choice;
-  
+  //method to store Basic cards
   this.addBasicCards = function () {
     var newBasicCard = new BasicCard(this.front, this.back);
     storeBC.push(newBasicCard);
   };
+  //method to store cloze cards
   this.addClozeCards = function () {
     console.log("gotten this far?");
     var newClozeCard = new ClozeCard(this.text, this.cloze);
@@ -25,6 +29,7 @@ function CardAdmin (front, back, text, cloze, choice) {
     }
   };
   this.showCards = function () {
+    //displays current cards stored for both types.
     if(this.choice === "Basic Card"){
       console.log("=======Currently Stored Basic Cards==============");
       console.log(storeBC);
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+/**
+**UCSD Bootcamp
+**Khoi Nguyen
+**4/7/2017
+**Homework #11
+**/
+//importing packages and files.
 var inquirer = require('inquirer');
 var CardAdmin = require('./CardAdmin.js');
-
+//Console greeting
 console.log("=====================================");
 console.log("Welcome to Flashcards Creator!!!    |");
 console.log("=====================================");
@@ -8,6 +15,7 @@ console.log("=====================================");
 //initial kickstart for app to begin sequence.
 start();
 
+//method to begin CLI.
 function start() {
   var startQuestion = [
     {
@@ -26,6 +34,7 @@ function start() {
       askClozeCardInfo(answer.userChoice);
   });
 }
+//method to ask the user for front and back of basic card
 function askBasicCardInfo(choice) {
   inquirer.prompt([
     {
@@ -49,7 +58,7 @@ function askBasicCardInfo(choice) {
     addAnother();
   });
 }
-
+//methode to ask the user for text and cloze.
 function askClozeCardInfo(choice) {
   inquirer.prompt([
      {
@@ -73,7 +82,7 @@ function askClozeCardInfo(choice) {
     addAnother();
   });
 }
-
+//final method to either add or quit the application.
 function addAnother () {
   inquirer.prompt([
       {
@@ -88,7 +97,7 @@ function addAnother () {
       }
       else{
         console.log("****************************************");
-        console.log("Thanks for using the Flash Cards App :)*");
+        console.log("Thanks for using the Flash Cards App!! *");
         console.log("****************************************");
         return;
       }
